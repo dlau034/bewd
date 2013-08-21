581d920680889bee9e294e9541de49c6f0776d4a
David Lau <bbc093@hotmail.com>
2013-08-21 19:57:14 -0400

    applied post setup instructions

--- class12/MovieApp/config/environments/production.rb
+++ class12/MovieApp/config/environments/production.rb
@@ -1,6 +1,8 @@
 MoviesApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # raise "Please at the following for devise:        config.action_mailer.default_url_options = { :host => 'localhost:3000' }"
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
